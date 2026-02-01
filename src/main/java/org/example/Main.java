5e88ee45963233e7b1324cf66c31f4979f92e14d
Champloo1402 <bankov1402@gmail.com>
2026-02-01 11:12:58 +0100

    JPA order database implementation

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,140 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import org.example.model.Client;
+import org.example.model.Order;
+import org.example.model.Product;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.Callable;
+
+public class Main {
+    static EntityManagerFactory emf;
+    static EntityManager em;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            emf = Persistence.createEntityManagerFactory("ordersPU");
+            em = emf.createEntityManager();
+
+            boolean running = true;
+            try {
+                while (running) {
+                    System.out.println("1: add client");
+                    System.out.println("2: add product");
+                    System.out.println("3: create order");
+                    System.out.println("4: exit");
+
+                    String s = sc.nextLine();
+
+                    switch (s) {
+                        case "1":
+                            addClient(sc);
+                            break;
+                        case "2":
+                            addProduct(sc);
+                            break;
+                        case "3":
+                            createOrder(sc);
+                            break;
+                        case "4":
+                            running = false;
+                            break;
+                    }
+
+                }
+            } finally {
+                if (em != null) em.close();
+                if (emf != null) emf.close();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
+        System.out.println("JPA shutdown complete");
+    }
+
+    private static void addClient(Scanner sc) {
+        System.out.println("Enter a name for a client: ");
+        String name = sc.nextLine();
+
+        System.out.println("Enter an email for a client: ");
+        String email = sc.nextLine();
+
+        performTransaction(() -> {
+            Client client = new Client(name, email);
+            em.persist(client);
+            return client;
+        });
+    }
+
+    private static void addProduct(Scanner sc) {
+        System.out.println("Enter a name for a product: ");
+        String name = sc.nextLine();
+
+        System.out.println("Enter price: ");
+        double price = sc.nextDouble();
+
+        sc.nextLine();
+
+        performTransaction(() -> {
+            Product product = new Product(name, price);
+            em.persist(product);
+            return product;
+        });
+
+    }
+
+    public static void createOrder(Scanner sc) {
+        System.out.println("Enter client's ID: ");
+        Long id = sc.nextLong();
+        Client client = em.find(Client.class, id);
+        if(client == null) throw new RuntimeException("Client not found");
+
+        sc.nextLine();
+
+        System.out.println("Enter product IDs (comma-separated): ");
+        String ids = sc.nextLine();
+        String[] parts = ids.split(",");
+        List<Product> products = new ArrayList<>();
+
+        for(String part : parts){
+            Long productId = Long.parseLong(part.trim());
+            Product product = em.find(Product.class, productId);
+            products.add(product);
+        }
+
+        performTransaction(() -> {
+            Order order = new Order(client, products);
+            em.persist(order);
+            return order;
+        });
+
+
+    }
+
+    private static <T> void performTransaction(Callable<T> action) {
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+        try {
+            T result = action.call();
+            transaction.commit();
+
+        } catch (Exception ex) {
+            if (transaction.isActive())
+                transaction.rollback();
+
+            throw new RuntimeException(ex);
         }
     }
+
+
 }
--- src/main/java/org/example/model/Order.java
+++ src/main/java/org/example/model/Order.java
@@ -1,4 +1,63 @@
 package org.example.model;
 
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private Client client;
+
+    @ManyToMany
+    @JoinTable(
+            name = "order_product",
+            joinColumns =  @JoinColumn(name = "order_id"),
+            inverseJoinColumns = @JoinColumn(name = "product_id")
+    )
+    private List<Product> products = new ArrayList<>();
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public Order() {
+    }
+
+    public Order(Client client, List<Product> products) {
+        this.client = client;
+        this.products = products;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", client=" + client +
+                ", products=" + products +
+                '}';
+    }
 }
